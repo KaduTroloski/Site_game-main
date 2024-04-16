bb3ffba0c7a31b226d0e62527d210cfdbae8d99d
KaduTroloski <kadupropodoski@gmail.com>
2024-04-16 04:27:07 -0300

    React Site

---
+++ src/App.tsx
@@ -0,0 +1,84 @@
+
+import { useState, ChangeEvent } from 'react';
+import './App.css'
+import axios from 'axios';
+import Infos from './Components/Infos';
+
+function App() {
+
+  const [inputText, setInputText] = useState("");
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+    }
+
+      const [Data, Setdata] = useState("");
+      
+      const VerifyUser = async () => {
+          await axios.get(`https://api-node-7vk8.onrender.com/user?search=${inputText}`)
+            .then(res => {
+                Setdata(JSON.stringify(res.data))
+               })
+              
+           
+          if(Data.length <= 2 || inputText.length == 0){ 
+              const style_input = document.getElementById("Place") as HTMLElement
+              const style_text = document.getElementById("Fail_text") as HTMLElement
+              style_input.style.border = '2px solid red';
+              style_text.style.display = 'flex'
+              
+          }
+           else {
+              const div1 = document.getElementById("Input") as HTMLElement
+              const div2 = document.getElementById("Infos") as HTMLElement
+                
+               if(div1.style.display != 'flex'){
+                      div1.style.display = 'none';
+                      div2.style.display = 'flex';
+  
+              }
+             
+          }
+      
+      }
+      let Replacedata = (Data.replace(/"/g, ""));
+      Replacedata = Replacedata.replace(/{/g, "")
+      Replacedata = Replacedata.replace(/}/g, "")
+      Replacedata = Replacedata.replace(/:/g, "")
+      Replacedata = Replacedata.replace(/]/g, "")
+      Replacedata = Replacedata.replace(/name_player/, "")
+      Replacedata = Replacedata.replace(/total_score/, "")
+      Replacedata = Replacedata.replace(/total_deaths/, "")
+      Replacedata = Replacedata.replace(/total_minuts/, "")
+      Replacedata = Replacedata.replace(/total_seconds/, "")
+      const SplitData  = Replacedata.split(",");
+  
+  
+        
+  return (
+    <>
+      <div id="Home">
+
+      <div id="Input">
+        <h2>Enter your Nickname</h2>
+        <input id='Place' type="text" placeholder="Nickname..." onChange={handleChange} value={inputText}></input>
+        <p id="Fail_text">User not fund</p>
+        <button onClick={VerifyUser} >Entrar</button>
+    </div>
+
+
+        <div id="Infos">
+            <h2>FrogCoin's</h2>
+
+              <Infos data={SplitData}></Infos>
+
+            </div>
+        
+        </div>
+      <div/>
+     </>
+
+    
+  )
+}
+
+export default App
---
+++ src/Components/Infos.tsx
@@ -0,0 +1,31 @@
+import '../App.css';
+
+
+ function Infos({data}:any){
+    return  (
+        <div className="box">
+
+        <div className="Score">
+           <h3>You Scored</h3>
+          <main className='Main_score'>
+            <h3>{data[2]}</h3>
+            <p>Coin's</p>
+          </main>
+        </div>
+        <div className="PlayerInfos">
+          <main className='Main_playerinfo'>
+            <h3 className='flex_one'>Nickname</h3>
+            <hr color='black' />
+            <p className='flex_one'> {data[1]} </p>
+            </main>
+                <ul>
+                    <li>Deaths: {data[3]}</li>
+                    <li>Game Time: {data[4]}:{data[5]}</li>
+                
+                </ul>
+                
+            </div>
+        </div>
+    )
+}
+export default Infos;
