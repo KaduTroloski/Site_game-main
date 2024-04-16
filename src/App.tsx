a18cb06d36a8aef3622d1b4ec774755e08490b66
KaduTroloski <kadupropodoski@gmail.com>
2024-04-16 04:38:06 -0300

    New header

--- src/App.tsx
+++ src/App.tsx
@@ -16,8 +16,8 @@ function App() {
       const VerifyUser = async () => {
           await axios.get(`https://api-node-7vk8.onrender.com/user?search=${inputText}`, {
             headers: {
-              "Access-Control-Allow-Origin": "https://api-node-7vk8.onrender.com/user",
-              "Access-Control-Allow-Methods": "POST, GET",
+              "Access-Control-Allow-Origin": "https://api-node-7vk8.onrender.com/user?search=",
+              "Access-Control-Allow-Methods": "GET",
               "Access-Control-Allow-Headers": "*",
               "Access-Control-Max-Age": 86400
 
