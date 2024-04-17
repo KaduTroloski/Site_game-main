beb884d6e793aa2cdbf8d8f76b8933b201209edf
KaduTroloski <kadupropodoski@gmail.com>
2024-04-17 02:42:09 -0300

    value

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
 
       <div id="Input">
         <h2>Enter your Nickname</h2>
-        <input id='Place' type="text" placeholder="Nickname..." onChange={evt => GetInputValue(evt)} ></input>
+        <input id='Place' type="text" placeholder="Nickname..." onChange={evt => GetInputValue(evt)} value={inputText} ></input>
         <p id="Fail_text">User not fund</p>
         <button onClick={VerifyUser} >Entrar</button>
     </div>
