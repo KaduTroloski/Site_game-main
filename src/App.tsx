0fb02c99f5c344922221735bf55d186424e50c30
KaduTroloski <kadupropodoski@gmail.com>
2024-04-17 02:26:14 -0300

    handlechange

--- src/App.tsx
+++ src/App.tsx
@@ -66,7 +66,7 @@ function App() {
 
       <div id="Input">
         <h2>Enter your Nickname</h2>
-        <input id='Place' type="text" placeholder="Nickname..." onBeforeInput={handleChange} value={inputText}></input>
+        <input id='Place' type="text" placeholder="Nickname..." onAbortCapture={handleChange} value={inputText}></input>
         <p id="Fail_text">User not fund</p>
         <button onClick={VerifyUser} >Entrar</button>
     </div>
