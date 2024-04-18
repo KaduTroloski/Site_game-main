0faf8c1abe9eb0d356ac9d82efeb749eac4339d8
KaduTroloski <kadupropodoski@gmail.com>
2024-04-18 03:45:22 -0300

    CSS

--- src/Components/Infos.tsx
+++ src/Components/Infos.tsx
@@ -1,12 +1,13 @@
 import '../App.css';
 
 
- function Infos({data}:any){
-    return  (
-        <div className="box">
+function Infos({ data }: any) {
+  return (
+    <>
+      <div className="box">
 
         <div className="Score">
-           <h3>You Scored</h3>
+          <h3>You Scored</h3>
           <main className='Main_score'>
             <h3>{data[2]}</h3>
             <p>Coin's</p>
@@ -17,15 +18,16 @@ import '../App.css';
             <h3 className='flex_one'>Nickname</h3>
             <hr color='black' />
             <p className='flex_one'> {data[1]} </p>
-            </main>
-                <ul>
-                    <li>Deaths: {data[3]}</li>
-                    <li>Game Time: {data[4]}:{data[5]}</li>
-                
-                </ul>
-                
-            </div>
+          </main>
+          <ul>
+            <li>Deaths: {data[3]}</li>
+            <li>Game Time: {data[4]}:{data[5]}</li>
+
+          </ul>
+
         </div>
-    )
+      </div>
+    </>
+  )
 }
 export default Infos;
