642f55ec144f3fd159aaf9233d3c92ae9045ae8b
KaduTroloski <kadupropodoski@gmail.com>
2024-04-18 05:31:18 -0300

    User fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,35 +5,45 @@ import axios from 'axios';
 import Infos from './Components/Infos';
 
 function App() {
+  const [inputText, setInputText] = useState('')
 
-  const [inputText, setInputText] = useState('');
+
+  const handleChange = (event) => {
+
+    setInputText(event.target.value)
+    console.log(inputText)
+  };
+
   const [Data, Setdata] = useState("");
 
   const VerifyUser = async () => {
+
     axios.defaults.headers.get['Content-Type'] = 'application/json;charset=utf-8';
     axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
     await axios.get(`https://api-node-7vk8.onrender.com/user?search=${inputText}`)
       .then(res => {
+
         Setdata(JSON.stringify(res.data))
       })
+      .finally(() => {
+        if (Data.length > 0 || inputText.length == 0) {
+          const style_input = document.getElementById("Place") as HTMLElement
+          const style_text = document.getElementById("Fail_text") as HTMLElement
+          style_input.style.border = '2px solid red';
+          style_text.style.display = 'flex'
+        }
+        else {
 
-    if (Data.length <= 2 || inputText.length == 0 && !Data) {
-      const style_input = document.getElementById("Place") as HTMLElement
-      const style_text = document.getElementById("Fail_text") as HTMLElement
-      style_input.style.border = '2px solid red';
-      style_text.style.display = 'flex'
-    }
-    else {
+          const div1 = document.getElementById("Input") as HTMLElement
+          const div2 = document.getElementById("Infos") as HTMLElement
 
-      const div1 = document.getElementById("Input") as HTMLElement
-      const div2 = document.getElementById("Infos") as HTMLElement
+          if (div1.style.display != 'flex') {
+            div1.style.display = 'none';
+            div2.style.display = 'flex';
 
-      if (div1.style.display != 'flex') {
-        div1.style.display = 'none';
-        div2.style.display = 'flex';
-
-      }
-    }
+          }
+        }
+      })
   }
 
 
@@ -57,7 +67,7 @@ function App() {
 
         <div id="Input">
           <h2>Enter your Nickname</h2>
-          <input id='Place' type="text" placeholder="Nickname..." onChange={event => setInputText(event.target.value)} ></input>
+          <input id='Place' type="text" name='Place' placeholder="Nickname..." onChange={handleChange} onMouseLeave={handleChange}></input>
           <p id="Fail_text">User not fund</p>
           <button onClick={VerifyUser} >Entrar</button>
         </div>
