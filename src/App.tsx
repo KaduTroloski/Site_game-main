77b3bcb4af5f9452bc3ceb0ac1e751b0d4b9586f
KaduTroloski <kadupropodoski@gmail.com>
2024-04-18 01:15:36 -0300

    cors

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App() {
   const VerifyUser = async () => {
     await axios.get(`https://api-node-7vk8.onrender.com/user?search=${inputText}`, {
       headers: {
-        "Access-Control-Allow-Origin": "https://api-node-7vk8.onrender.com",
+        "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET",
         "Access-Control-Allow-Headers": "*",
       }
